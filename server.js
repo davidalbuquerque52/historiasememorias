15f42ea936981d72114eb995d84c3a81153090fb
Allan Albuquerque <allandavidcomode@gmail.com>
2022-06-05 00:32:27 -0300

    ajuste de porta

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/dist/historiasememorias/index.html');
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
